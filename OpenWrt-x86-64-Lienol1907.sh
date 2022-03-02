3c2710d70218d3fde33ae032cf8700657b146a00
Haoyan2046 <liuzh10@smu.edu.cn>
2022-03-02 22:15:29 +0800

    Update OpenWrt-x86-64-Lienol1907.sh

--- OpenWrt-x86-64-Lienol1907.sh
+++ OpenWrt-x86-64-Lienol1907.sh
@@ -27,12 +27,12 @@ svn co https://github.com/openwrt/openwrt/trunk/package/libs/libcap feeds/packag
 # 自定义定制选项
 ZZZ="package/default-settings/files/zzz-default-settings"
 #
-sed -i 's#192.168.1.1#192.168.0.1#g' package/base-files/files/bin/config_generate 	      #定制默认IP
+sed -i 's#192.168.50.253#192.168.0.1#g' package/base-files/files/bin/config_generate 	      #定制默认IP
 sed -i 's#max-width:200px#max-width:1000px#g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm #修改首页样式
 sed -i 's#max-width:200px#max-width:1000px#g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index_x86.htm #修改X86首页样式
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' $ZZZ                                             # 取消系统默认密码
 sed -i "/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-X86'" $ZZZ       # 修改主机名称为OpenWrt-X86
-sed -i "s/OpenWrt /Jinlife build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ          # 增加自己个性名称
+sed -i "s/OpenWrt / build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ          # 增加自己个性名称
 
 sed -i 's#option commit_interval 24h#option commit_interval 60m#g' feeds/packages/net/nlbwmon/files/nlbwmon.config #修改流量统计写入为60分钟
 sed -i 's#option database_directory /var/lib/nlbwmon#option database_directory /etc/config/nlbwmon_data#g' feeds/packages/net/nlbwmon/files/nlbwmon.config #修改流量统计数据存放默认位置
@@ -87,7 +87,7 @@ EOF
 # 设置固件大小:
 cat >> .config <<EOF
 CONFIG_TARGET_KERNEL_PARTSIZE=16
-CONFIG_TARGET_ROOTFS_PARTSIZE=160
+CONFIG_TARGET_ROOTFS_PARTSIZE=500
 EOF
 
 # 固件压缩:
@@ -134,7 +134,7 @@ EOF
 # 第三方插件选择:
 cat >> .config <<EOF
 # CONFIG_PACKAGE_luci-app-oaf=y #应用过滤
-# CONFIG_PACKAGE_luci-app-openclash=y #OpenClash客户端
+CONFIG_PACKAGE_luci-app-openclash=y #OpenClash客户端
 # CONFIG_PACKAGE_luci-app-serverchan=y #微信推送
 # CONFIG_PACKAGE_luci-app-eqos=y #IP限速
 CONFIG_PACKAGE_luci-app-poweroff=y #关机（增加关机功能）
@@ -143,51 +143,51 @@ CONFIG_PACKAGE_luci-app-autotimeset=y #定时重启系统，网络
 EOF
 
 # Passwall插件:
-cat >> .config <<EOF
-CONFIG_PACKAGE_luci-app-passwall=y
-CONFIG_PACKAGE_https-dns-proxy=y
-CONFIG_PACKAGE_naiveproxy=y
-CONFIG_PACKAGE_kcptun-client=y
-CONFIG_PACKAGE_chinadns-ng=y
-CONFIG_PACKAGE_brook=y
-CONFIG_PACKAGE_trojan-go=y
-EOF
+# cat >> .config <<EOF
+# CONFIG_PACKAGE_luci-app-passwall=y
+# CONFIG_PACKAGE_https-dns-proxy=y
+# CONFIG_PACKAGE_naiveproxy=y
+# CONFIG_PACKAGE_kcptun-client=y
+# CONFIG_PACKAGE_chinadns-ng=y
+# CONFIG_PACKAGE_brook=y
+# CONFIG_PACKAGE_trojan-go=y
+# EOF
 
 # 常用LuCI插件:
 cat >> .config <<EOF
 CONFIG_PACKAGE_luci-app-webadmin=y #Web管理页面设置
 CONFIG_PACKAGE_luci-app-filetransfer=y #系统-文件传输
 CONFIG_PACKAGE_luci-app-upnp=y #通用即插即用UPnP(端口自动转发)
-CONFIG_PACKAGE_luci-app-vlmcsd=y #KMS激活服务器
-CONFIG_PACKAGE_luci-app-ddns=y #DDNS服务
+# CONFIG_PACKAGE_luci-app-vlmcsd=y #KMS激活服务器
+# CONFIG_PACKAGE_luci-app-ddns=y #DDNS服务
 # CONFIG_PACKAGE_luci-app-control-mia=y #时间控制
 CONFIG_PACKAGE_luci-app-control-webrestriction=y #访问限制
 CONFIG_PACKAGE_luci-app-control-weburl=y #网址过滤
 CONFIG_PACKAGE_luci-app-nlbwmon=y #宽带流量监控
 CONFIG_PACKAGE_luci-app-wrtbwmon=y #实时流量监测
-CONFIG_PACKAGE_luci-app-xlnetacc=y #迅雷快鸟
+# CONFIG_PACKAGE_luci-app-xlnetacc=y #迅雷快鸟
 CONFIG_PACKAGE_luci-app-arpbind=y #IP/MAC ARP绑定
 CONFIG_PACKAGE_luci-app-adguardhome=y #ADguardHome去广告服务
 CONFIG_PACKAGE_luci-app-socat=y #IPV6 端口映射 IPV4
 
 CONFIG_PACKAGE_luci-app-sfe=n #高通开源的 Shortcut FE 转发加速引擎
 CONFIG_PACKAGE_luci-app-flowoffload=n #开源 Linux Flow Offload 驱动
-CONFIG_PACKAGE_luci-app-frpc=n #Frp内网穿透
+# CONFIG_PACKAGE_luci-app-frpc=n #Frp内网穿透
 CONFIG_PACKAGE_luci-app-autoreboot=n #定时重启
-CONFIG_PACKAGE_luci-app-adbyby-plus=n #adbyby去广告
+# CONFIG_PACKAGE_luci-app-adbyby-plus=n #adbyby去广告
 CONFIG_PACKAGE_luci-app-wol=n #网络唤醒
 CONFIG_PACKAGE_luci-app-control-timewol=n #定时唤醒
-CONFIG_PACKAGE_luci-app-smartdns=n #smartdns服务器
+# CONFIG_PACKAGE_luci-app-smartdns=n #smartdns服务器
 CONFIG_PACKAGE_luci-app-diskman=n #磁盘管理磁盘信息
 CONFIG_PACKAGE_luci-app-unblockmusic=n #解锁网易云灰色歌曲
 CONFIG_PACKAGE_luci-app-unblockneteasemusic-go=n #解锁网易云灰色歌曲
 CONFIG_PACKAGE_luci-app-unblockneteasemusic-mini=n #解锁网易云灰色歌曲
-CONFIG_PACKAGE_luci-app-usb-printer=n #USB打印机
+# CONFIG_PACKAGE_luci-app-usb-printer=n #USB打印机
 CONFIG_PACKAGE_luci-app-mwan3helper=n #多拨负载均衡
 CONFIG_PACKAGE_luci-app-mwan3=n #多线多拨
-CONFIG_PACKAGE_luci-app-hd-idle=n #磁盘休眠
-CONFIG_PACKAGE_luci-app-zerotier=n #Zerotier内网穿透
-CONFIG_PACKAGE_luci-app-sqm=n #SQM智能队列管理
+# CONFIG_PACKAGE_luci-app-hd-idle=n #磁盘休眠
+# CONFIG_PACKAGE_luci-app-zerotier=n #Zerotier内网穿透
+# CONFIG_PACKAGE_luci-app-sqm=n #SQM智能队列管理
 #
 # passwall相关(禁用):
 #
@@ -198,21 +198,21 @@ CONFIG_PACKAGE_luci-app-ipsec-vpnserver-manyusers=n #ipsec VPN服务
 CONFIG_PACKAGE_luci-app-pppoe-relay=n #PPPoE穿透
 CONFIG_PACKAGE_luci-app-pppoe-server=n #PPPoE服务器
 CONFIG_PACKAGE_luci-app-pptp-vpnserver-manyusers=n #PPTP VPN 服务器
-CONFIG_PACKAGE_luci-app-trojan-server=n #Trojan服务器
-CONFIG_PACKAGE_luci-app-v2ray-server=n #V2ray服务器
-CONFIG_PACKAGE_luci-app-brook-server=n #brook服务端
-CONFIG_PACKAGE_luci-app-ssr-libev-server=n #ssr-libev服务端
-CONFIG_PACKAGE_luci-app-ssr-python-pro-server=n #ssr-python服务端
-CONFIG_PACKAGE_luci-app-kcptun=n #Kcptun客户端
-CONFIG_PACKAGE_luci-app-softethervpn=n #SoftEtherVPN服务器
+# CONFIG_PACKAGE_luci-app-trojan-server=n #Trojan服务器
+# CONFIG_PACKAGE_luci-app-v2ray-server=n #V2ray服务器
+# CONFIG_PACKAGE_luci-app-brook-server=n #brook服务端
+# CONFIG_PACKAGE_luci-app-ssr-libev-server=n #ssr-libev服务端
+# CONFIG_PACKAGE_luci-app-ssr-python-pro-server=n #ssr-python服务端
+# CONFIG_PACKAGE_luci-app-kcptun=n #Kcptun客户端
+# CONFIG_PACKAGE_luci-app-softethervpn=n #SoftEtherVPN服务器
 #
 # 文件共享相关(禁用):
 #
-CONFIG_PACKAGE_luci-app-aria2=n #Aria2离线下载
+# CONFIG_PACKAGE_luci-app-aria2=n #Aria2离线下载
 CONFIG_PACKAGE_luci-app-minidlna=n #miniDLNA服务
-CONFIG_PACKAGE_luci-app-kodexplorer=n #可到私有云
-CONFIG_PACKAGE_luci-app-filebrowser=n #File Browser私有云
-CONFIG_PACKAGE_luci-app-fileassistant=n #文件助手
+# CONFIG_PACKAGE_luci-app-kodexplorer=n #可到私有云
+# CONFIG_PACKAGE_luci-app-filebrowser=n #File Browser私有云
+# CONFIG_PACKAGE_luci-app-fileassistant=n #文件助手
 CONFIG_PACKAGE_luci-app-vsftpd=n #FTP 服务器
 CONFIG_PACKAGE_luci-app-samba=n #网络共享
 CONFIG_PACKAGE_autosamba=n #网络共享
